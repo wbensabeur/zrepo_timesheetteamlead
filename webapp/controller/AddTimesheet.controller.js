84254e4f3af2b13be225610fa80c25bd6243dc93
Vijay Singh Rajput <vijay.singh.rajput@sap.com>
2017-07-26 18:17:49 +0000

    Add Mutiple Time Screen

--- webapp/controller/AddTimesheet.controller.js
+++ webapp/controller/AddTimesheet.controller.js
@@ -2,8 +2,8 @@ sap.ui.define([
 	"com/vinci/timesheet/admin/controller/BaseController",
 	"sap/ui/model/json/JSONModel",
 	"com/vinci/timesheet/admin/model/formatter",
-		"com/vinci/timesheet/admin/utility/datetime"
-], function(BaseController, JSONModel, formatter,datetime) {
+	"com/vinci/timesheet/admin/utility/datetime"
+], function(BaseController, JSONModel, formatter, datetime) {
 	"use strict";
 	return BaseController.extend("com.vinci.timesheet.admin.controller.AddTimesheet", {
 		formatter: formatter,
@@ -14,43 +14,57 @@ sap.ui.define([
 		 */
 		onInit: function() {
 			this.getRouter().getRoute("AddTimesheet").attachPatternMatched(this._onObjectMatched, this);
+			var oFragment = sap.ui.xmlfragment(this.getView().getId(), "com.vinci.timesheet.admin.view.AddProjectTime", this);
+			this.getView().byId('addTimeTab').addContent(oFragment);
+		
 		},
-			/**
-			 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-			 * (NOT before the first rendering! onInit() is used for that one!).
-			 * @memberOf com.vinci.timesheet.admin.view.AddTimesheet
-			 */
-			//	onBeforeRendering: function() {
-			//
-			//	},
-			/**
-			 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-			 * This hook is the same one that SAPUI5 controls get after being rendered.
-			 * @memberOf com.vinci.timesheet.admin.view.AddTimesheet
-			 */
-			//	onAfterRendering: function() {
-			//
-			//	},
-			/**
-			 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-			 * @memberOf com.vinci.timesheet.admin.view.AddTimesheet
-			 */
-			//	onExit: function() {
-			//
-			//	}
-			
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf com.vinci.timesheet.admin.view.AddTimesheet
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf com.vinci.timesheet.admin.view.AddTimesheet
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf com.vinci.timesheet.admin.view.AddTimesheet
+		 */
+		//	onExit: function() {
+		//
+		//	}
 		/**
 		 *@memberOf com.vinci.timesheet.admin.controller.AddTimesheet
 		 */
-		
 		_onObjectMatched: function(oEvent) {
 			this.userPref = this.getView().getModel("userPreference").getData();
 			var caldenderdata = datetime.getCalenderData(this.userPref.startDate, this.userPref.defaultPeriod, this.getResourceBundle());
 			var oCalendarModel = new JSONModel(caldenderdata);
 			this.setModel(oCalendarModel, "calendar");
-		}, 
+		},
 		onPressCancel: function() {
-			this.getRouter().navTo("periodSelection", {	}, true);
+			this.getRouter().navTo("periodSelection", {}, true);
+		},
+		/**
+		 *@memberOf com.vinci.timesheet.admin.controller.AddTimesheet
+		 */
+		OnaddNewHourPress: function(oEvent) {
+			
+			var oFragment = sap.ui.xmlfragment(this.getView().getId(), "com.vinci.timesheet.admin.view.AddProjectTime", this);
+		    
+			this.getView().byId('addTimeTab').addContent(oFragment);
+		},
+		OnTimeDelete : function(oEvent) {
+			var sourceContent = oEvent.getSource().getParent().getParent();
+			this.getView().byId('addTimeTab').removeContent(sourceContent);
 		}
 	});
 });
