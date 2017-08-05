6fa6ef19612329b1fc909d37bf89e2e125e0531e
Vijay Singh Rajput <vijay.singh.rajput@sap.com>
2017-08-05 19:21:35 +0000

    SelectProject and ProjectSearch Fragment

--- webapp/controller/AddTimesheet.controller.js
+++ webapp/controller/AddTimesheet.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 	"com/vinci/timesheet/admin/utility/datetime",
 	"com/vinci/timesheet/admin/utility/fragment",
 	"sap/m/MessageBox"
-], function(BaseController, JSONModel, formatter, datetime,fragment,MessageBox) {
+], function(BaseController, JSONModel, formatter, datetime, fragment, MessageBox) {
 	"use strict";
 	return BaseController.extend("com.vinci.timesheet.admin.controller.AddTimesheet", {
 		formatter: formatter,
@@ -17,29 +17,28 @@ sap.ui.define([
 		onInit: function() {
 			this.getRouter().getRoute("AddTimesheet").attachPatternMatched(this._onObjectMatched, this);
 			var oFragment = sap.ui.xmlfragment(this.getView().getId(), "com.vinci.timesheet.admin.view.AddProjectTime", this);
-			this.getView().byId('addTimeTab').getContent()[0].addItem(oFragment);
+			this.getView().byId('addTimeTab').getItems()[0].addItem(oFragment);
 
 			var odata = {
 				totalhrs: 0,
 				visibleHrs: true,
 				visibleDailyAllow: true,
-				visibleKM:true,
-				visibleAbsence:false,
-				visibleEquipment:false,
-				visibleSummary:false,
+				visibleKM: true,
+				visibleAbsence: false,
+				visibleEquipment: false,
+				visibleSummary: false,
 				visibleProjectOptional: false,
-				newTime : true
+				newTime: true
 			};
 			var oModel = new JSONModel(odata);
 			this.getView().setModel(oModel, "AddTime");
-			
+
 			var projectdata = {
-				worklistTableTitle : this.getResourceBundle().getText("projectSearchHeader")
+				worklistTableTitle: this.getResourceBundle().getText("projectSearchHeader")
 			};
-			
+
 			var oProjectModel = new JSONModel(projectdata);
 			this.getView().setModel(oProjectModel, "projectSearch");
-			
 
 		},
 		/**
@@ -78,20 +77,25 @@ sap.ui.define([
 			this.getRouter().navTo("periodSelection", {}, true);
 		},
 		onPressProjectCancel: function() {
-			this._backtoMainScreen();
+			var projectfragment = this._getProjectSearchFragment();
+			var visible = [this.getView().byId('MainCancelButton'),this.getView().byId('MainAddButton')];
+			var hide = [this.getView().byId('ProjectCancelButton'),this.getView().byId('ProjectSelectButton')];
+			fragment.SearchProject_destroy(projectfragment,hide,visible);
 
 		},
 		onPressProjectSelect: function(oEvent) {
-			var projectContext = oEvent.getSource().getCustomData()[0].getValue();
-			if(projectContext === null || projectContext === "")
-			{
+			var projectfragment = this._getProjectSearchFragment();
+			var projectContext = fragment.SearchProject_getProjectContext(projectfragment); //oEvent.getSource().getCustomData()[0].getValue();
+			if (projectContext === null || projectContext === "") {
 				MessageBox.alert(this.getResourceBundle().getText("noprojectselectmessage"));
+			} else {
+				var visible = [this.getView().byId('MainCancelButton'),this.getView().byId('MainAddButton')];
+				var hide = [this.getView().byId('ProjectCancelButton'),this.getView().byId('ProjectSelectButton')];
+				
+				fragment.SelectProject_afterSelection(projectContext);
+				fragment.SearchProject_destroy(projectfragment,hide,visible);
+
 			}
-			this.currentLabel.bindElement(projectContext);
-			this.currentLabel.setVisible(true);
-			this.ownIntialButton.setVisible(false);
-			this.ownRefreshButton.setVisible(true);
-			this._backtoMainScreen();
 		},
 		/**
 		 *@memberOf com.vinci.timesheet.admin.controller.AddTimesheet
@@ -100,13 +104,13 @@ sap.ui.define([
 
 			var oFragment = sap.ui.xmlfragment(this.getView().getId(), "com.vinci.timesheet.admin.view.AddProjectTime", this);
 
-			this.getView().byId('addTimeTab').getContent()[0].addItem(oFragment);
+			this.getView().byId('addTimeTab').getItems()[0].addItem(oFragment);
 		},
 		OnTimeDelete: function(oEvent) {
 			var source = oEvent.getSource();
 			//var sourcePanel = source.getParent().getParent();
 			var sourcePanel = this._getOwnPanelObject(source);
-			this.getView().byId('addTimeTab').getContent()[0].removeItem(sourcePanel);
+			this.getView().byId('addTimeTab').getItems()[0].removeItem(sourcePanel);
 			var currentValue = sourcePanel.getCustomData()[0].getValue();
 			var currentTotalhrs = this.getView().getModel('AddTime').getProperty('/totalhrs');
 			var newTotalhrs = currentTotalhrs - currentValue;
@@ -149,80 +153,71 @@ sap.ui.define([
 			this.getView().getModel('AddTime').setProperty('/totalhrs', newTotalhrs);
 			sourcePanel.getCustomData()[0].setValue(newValue);
 		},
-		
-		//// **Project Fragment Event** ///////
+
+		//// **SearchProject Fragment Event** ///////
 		OnProjectSelected: function(oEvent) {
 			var selectButton = this.getView().byId('ProjectSelectButton');
 			fragment.SearchProject_OnProjectSelected(oEvent, selectButton);
-			/*var contextPath = oEvent.getParameter('listItem').getBindingContext().getPath();
-			oEvent.getSource().getParent().getCustomData()[0].setValue(contextPath);
-			this.currentProjectContext = contextPath;*/
-			//var label = sap.ui.getCore().byId(oEvent.getSource().getParent().getCustomData()[1].getValue());
-			//label.bindElement(contextPath);
-
 		},
+		OnFavoriteChange: function(oEvent) {
+			fragment.SearchProject_OnFavoriteChange(oEvent, this.getView().getModel());
+		},
+		onProjectSearchFinished: function(oEvent) {
+			fragment.SearchProject_onProjectSearchFinished(oEvent, this.getModel("projectSearch"), this.getResourceBundle()) ;
+		},
+
+		////****SearchProject Fragment Event End******//////////
 		
-		////****Project Fragment Event End******//////////
+		//// **SelectProject Fragment Event** ///////
 		OnProjectSearch: function(oEvent) {
-			
-			
 
 			var ownHBox = oEvent.getSource().getParent();
 			var ownLabel = ownHBox.getItems()[0];
 			this.ownIntialButton = ownHBox.getItems()[1];
 			this.ownRefreshButton = ownHBox.getItems()[2];
 			var content = this._getOwnContentObject(ownHBox);
-			/*if(content.length < 2)
-			{
-				var oFragment = sap.ui.xmlfragment(this.getView().getId(), "com.vinci.timesheet.admin.view.SelectProject", this);
-				content.push(oFragment);
-			}*/
 			
-			content[0].setVisible(false); // main Frame
-			content[1].setVisible(true); // Project SEarch Frame
-			content[1].getCustomData()[1].setValue(ownLabel.getId());
 			this.currentLabel = ownLabel;
 			// Visible = false
-			this.getView().byId('MainCancelButton').setVisible(false);
-			this.getView().byId('MainAddButton').setVisible(false);
+			var hide = [this.getView().byId('MainCancelButton'),this.getView().byId('MainAddButton')];
+			//this.getView().byId('MainCancelButton').setVisible(false);
+			//this.getView().byId('MainAddButton').setVisible(false);
 
 			// Visible = true
-			this.getView().byId('ProjectCancelButton').setVisible(true);
-			this.getView().byId('ProjectSelectButton').setVisible(true);
+			var visible = [this.getView().byId('ProjectCancelButton'),this.getView().byId('ProjectSelectButton')];
+			//this.getView().byId('ProjectCancelButton').setVisible(true);
+			//this.getView().byId('ProjectSelectButton').setVisible(true);
+		fragment.SelectProject_OnProjectSearch(oEvent, this, content[0], this.getView().byId('ProjectSelectButton'), hide, visible);	
+		//fragment.SearchProject_init(this , content[0],ownLabel,this.getView().byId('ProjectSelectButton'), hide, visible);
+			//fragment.SearchProject_init(fragment, content[0], ownLabel, this.getView().byId('ProjectSelectButton'), hide, visible);
 
 		},
-		onProjectSearchFinished : function (oEvent) {
-			var sTitle, oTable = oEvent.getSource(),
-				iTotalItems = oEvent.getParameter("total");
-			// only update the counter if the length is final and
-			// the table is not empty
-			if (iTotalItems && oTable.getBinding("items").isLengthFinal()) {
-				sTitle = this.getResourceBundle().getText("projectSearchHeaderCount", [iTotalItems]);
-			} else {
-				sTitle = this.getResourceBundle().getText("projectSearchHeader");
-			}
-			this.getModel("projectSearch").setProperty("/worklistTableTitle", sTitle);
-			
-		},
-		OnProjectRefresh : function(oEvent) {
+		OnProjectRefresh: function(oEvent) {
 			var ownHBox = oEvent.getSource().getParent();
 			var ownLabel = ownHBox.getItems()[0];
 			var content = this._getOwnContentObject(ownHBox);
-			
-			content[1].getCustomData()[1].setValue(ownLabel.getId());
+
+			//content[1].getCustomData()[1].setValue(ownLabel.getId());
 			this.currentLabel = ownLabel;
-			content[0].setVisible(false); // main Frame
-			content[1].setVisible(true); // Project SEarch Frame
+			//content[0].setVisible(false); // main Frame
+			//content[1].setVisible(true); // Project SEarch Frame
 			// Visible = false
-			this.getView().byId('MainCancelButton').setVisible(false);
-			this.getView().byId('MainAddButton').setVisible(false);
+			var hide = [this.getView().byId('MainCancelButton'),this.getView().byId('MainAddButton')];
+			//this.getView().byId('MainCancelButton').setVisible(false);
+			//this.getView().byId('MainAddButton').setVisible(false);
 
 			// Visible = true
-			this.getView().byId('ProjectCancelButton').setVisible(true);
-			this.getView().byId('ProjectSelectButton').setVisible(true);
-
+			//this.getView().byId('ProjectCancelButton').setVisible(true);
+			//this.getView().byId('ProjectSelectButton').setVisible(true);
+			var visible = [this.getView().byId('ProjectCancelButton'),this.getView().byId('ProjectSelectButton')];
 			
+			fragment.SelectProject_OnProjectRefresh(oEvent, this, content[0], this.getView().byId('ProjectSelectButton'), hide, visible);	
+
 		},
+		//// **SelectProject Fragment Event End** ///////
+		
+		
+		
 		OnTabSelected: function(oEvent) {
 			var key = oEvent.getParameter('key');
 			if (key === 'allowance') {
@@ -231,23 +226,7 @@ sap.ui.define([
 				this.getView().getModel('AddTime').setProperty('/visibleProjectOptional', false);
 			}
 		},
-		OnFavoriteChange: function(oEvent) {
-			var icon = oEvent.getSource();
-			var currentState = this.getView().getModel().getProperty(icon.getBindingContext().getPath()+'/Favorite');
-			if(currentState) // to become unfav
-			{
-				this.getView().getModel().setProperty(icon.getBindingContext().getPath()+'/Favorite',false);
-				
-				this.getView().getModel().update(icon.getBindingContext().getPath(),{Favorite:false});
-			
-			}
-			else // to become Fav
-			{
-				this.getView().getModel().setProperty(icon.getBindingContext().getPath()+'/Favorite',true);
-				this.getView().getModel().update(icon.getBindingContext().getPath(),{Favorite:true});
-			}
-			
-		},
+
 		_getOwnPanelObject: function(source) {
 			var parent = source.getParent();
 
@@ -270,7 +249,7 @@ sap.ui.define([
 		_getOwnHBox: function(source) {
 
 		},
-		_backtoMainScreen: function() {
+		/*_backtoMainScreen: function() {
 
 			var tabItems = this.getView().byId('idIconTabBarMulti').getItems();
 
@@ -287,7 +266,7 @@ sap.ui.define([
 			// Visible = false
 			this.getView().byId('ProjectCancelButton').setVisible(false);
 			this.getView().byId('ProjectSelectButton').setVisible(false);
-		},
+		},*/
 		_getOwnAllDayComboBox: function(radioGroup) {
 			var parent = radioGroup.getParent();
 			while (parent.getMetadata().getName() !== 'sap.m.HBox') {
@@ -305,6 +284,19 @@ sap.ui.define([
 		},
 		_getOwnSelectedHrContent: function(radioGroup) {
 			return radioGroup.getParent().getParent().getParent().getItems()[3];
-		}
+		},
+		_setProjectSearchFragment:function (frameid) {
+			this.projectSearchFragment = frameid;
+		},
+		_getProjectSearchFragment:function(){
+			var projectfragment = sap.ui.getCore().byId(this.projectSearchFragment);
+			return projectfragment;
+		}/*,
+		_setSelectProjectContext: function (data){
+			this.SelectProjectContext = data;
+		},
+		_getSelectProjectContext: function(){
+			return this.SelectProjectContext;
+		}*/
 	});
 });
--- webapp/utility/fragment.js
+++ webapp/utility/fragment.js
@@ -2,23 +2,106 @@ sap.ui.define([], function() {
 	"use strict";
 
 	return {
-		SearchProject_init: function (selectButton){
-			selectButton.getCustomData()[0].getValue();
-			
+		SearchProject_init: function(controler, container, selectButton, hideElemnt, visibleElement) {
+
+			var fragment = sap.ui.xmlfragment(controler.getView().getId(), "com.vinci.timesheet.admin.view.SearchProject", controler);
+			controler._setProjectSearchFragment(fragment.getId());
+			//	fragment.getCustomData()[1].setValue(returnRef.getId()); 
+
+			var hideContainer = container.getItems()[0];
+			hideContainer.setVisible(false);
+			container.addItem(fragment);
+
+			selectButton.getCustomData()[0].setValue("");
+
+			// Hide Element from previous screen
+			for (var k = 0; k < hideElemnt.length; k++) {
+				hideElemnt[k].setVisible(false);
+			}
+
+			// Visible Element from Project Search
+			for (var l = 0; l < hideElemnt.length; l++) {
+				visibleElement[l].setVisible(true);
+			}
+
 		},
-		SearchProject_OnProjectFilterchange: function (){
-			
+		SearchProject_destroy: function(fragmentObject, hideElemnt, visibleElement) {
+			//var currentProjectContext = fragment.getCustomData()[0].getValue("");
+			var container = fragmentObject.getParent();
+			var hideContainer = container.getItems()[0];
+			hideContainer.setVisible(true);
+			fragmentObject.destroy(true);
+			// Hide Element from previous screen
+			for (var k = 0; k < hideElemnt.length; k++) {
+				hideElemnt[k].setVisible(false);
+			}
+
+			// Visible Element from Project Search
+			for (var l = 0; l < hideElemnt.length; l++) {
+				visibleElement[l].setVisible(true);
+			}
+
 		},
-		SearchProject_OnProjectSelected: function (oEvent,selectButton){
+		SearchProject_getProjectContext: function(fragmentObject) {
+			return fragmentObject.getCustomData()[0].getValue();
+		},
+		SearchProject_OnProjectFilterchange: function() {
+
+		},
+		SearchProject_OnProjectSelected: function(oEvent, selectButton) {
 			var contextPath = oEvent.getParameter('listItem').getBindingContext().getPath();
 			oEvent.getSource().getParent().getCustomData()[0].setValue(contextPath);
 			selectButton.getCustomData()[0].setValue(contextPath);
-			
+
 		},
-		SearchProject_OnFavoriteChange: function (){
-			
+		SearchProject_OnFavoriteChange: function(oEvent, model) {
+			var icon = oEvent.getSource();
+			var currentState = model.getProperty(icon.getBindingContext().getPath() + '/Favorite');
+			if (currentState) // to become unfav
+			{
+				model.setProperty(icon.getBindingContext().getPath() + '/Favorite', false);
+				model.update(icon.getBindingContext().getPath(), {
+					Favorite: false
+				});
+
+			} else // to become Fav
+			{
+				model.setProperty(icon.getBindingContext().getPath() + '/Favorite', true);
+				model.update(icon.getBindingContext().getPath(), {
+					Favorite: true
+				});
+			}
+		},
+		SearchProject_onProjectSearchFinished: function(oEvent, projectModel, resourceModel) {
+			var sTitle, oTable = oEvent.getSource(),
+				iTotalItems = oEvent.getParameter("total");
+			// only update the counter if the length is final and
+			// the table is not empty
+			if (iTotalItems && oTable.getBinding("items").isLengthFinal()) {
+				sTitle = resourceModel.getText("projectSearchHeaderCount", [iTotalItems]);
+			} else {
+				sTitle = resourceModel.getText("projectSearchHeader");
+			}
+			projectModel.setProperty("/worklistTableTitle", sTitle);
+		},
+		SelectProject_afterSelection: function(projectContext) {
+			this.selectProjectcontext[0].bindElement(projectContext); // Label
+			this.selectProjectcontext[0].setVisible(true);           // Label
+			this.selectProjectcontext[1].setVisible(false);         // ownIntialButton
+			this.selectProjectcontext[2].setVisible(true);        // ownRefreshButton
+		},
+		SelectProject_OnProjectSearch: function(oEvent, controler, container, selectButton, hideElemnt, visibleElement) {
+
+			var ownHBox = oEvent.getSource().getParent();
+			this.selectProjectcontext = ownHBox.getItems();
+			this.SearchProject_init(controler, container, selectButton, hideElemnt, visibleElement);
+		},
+		SelectProject_OnProjectRefresh: function(oEvent, controler, container, selectButton, hideElemnt, visibleElement) {
+			this.SearchProject_init(controler, container, selectButton, hideElemnt, visibleElement);
 		}
 		
+		
+
 	};
 
 });
