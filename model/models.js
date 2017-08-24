86cf7334b0666979bd73ac853e755c735c300419
Vijay Singh Rajput <vijay.singh.rajput@sap.com>
2017-08-24 08:20:23 +0000

    Import content for version 1.0.37.SNAPSHOT

---
+++ model/models.js
@@ -0,0 +1,50 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device"
+], function(JSONModel, Device) {
+	"use strict";
+
+	return {
+
+		createDeviceModel: function() {
+			var oModel = new JSONModel(Device);
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+		createUserPersolisationModel: function() {
+			var startDate = null; /// =datetime.getLastWeek(new Date()); in case of default 2 week display
+			var defaultBU = null;
+			var defaultPeriod = null;
+		
+			var userPref = {
+				defaultBU:defaultBU,
+				defaultPeriod:defaultPeriod,
+				employeeFilter:null,
+				startDate:startDate,
+				userID:null
+			};
+			var oModel = new JSONModel(userPref);
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+		createEmployeeSelection: function () {
+			var startDate = new Date();
+			var data = {
+				startDate:startDate,
+				employees:[]
+			};
+			var oModel = new JSONModel(data);
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+		createEmployeeDaySelection: function () {
+			
+			var data = [];
+			var oModel = new JSONModel(data);
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		}
+
+	};
+
+});
