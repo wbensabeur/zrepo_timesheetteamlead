68a295c8f227af0ccbd305762765b58f29df6267
Rajiv Shivdev Pandey <rajiv.shivdev.pandey@sap.com>
2017-10-12 09:01:01 +0000

    Changes for Team Manage

--- webapp/controller/TeamManage.controller.js
+++ webapp/controller/TeamManage.controller.js
@@ -84,37 +84,6 @@ sap.ui.define([
 			this.getModel("teamHeader").setProperty("/data/0/ColumnTxt1", sTitle);
 			this.getModel("teamHeader").setProperty("/data/0/ColumnTxt2", this.getModel("userPreference").getProperty("/defaultBUT"));
 			this.teamItemBinding();
-			if (this.refresh) {
-				for (var k = 0; k < this.selectedBox.length; k++) {
-					if (this.selectedBox[k].getCustomData().length > 0) {
-						this.selectedBox[k].getCustomData()[0].setValue("");
-					}
-				}
-				this.selectedBox = [];
-				this.employees = [];
-				this.getView().getModel("employeeDaysSelected").setData(this.employees);
-				this.refresh = false;
-				this.daySelected = [];
-			} else {
-				for (var l = 0; l < this.daySelected.length; l++) {
-					var source = this.daySelected[l];
-					var headerSeq = source.getParent().getParent().getInitialOrder();
-					source.data('background'); //getCustomData()[0].setValue("S");
-					source.getParent().data('background'); //.getCustomData()[0].setValue("S");
-					var Items = this.getView().byId('table').getItems();
-					for (var k1 = 0; k1 < Items.length; k1++) {
-						var button = Items[k1].getCells()[headerSeq];
-						if (button.getEnabled()) {
-							button.getCustomData()[0].setValue("S");
-						}
-					}
-				}
-			}
-			this.storeAllEmployee();
-
-			/*var col1 = oTable.getColumns()[0].getWidth();
-			alert(col1);*/
-
 		},
 		OnHourPress: function(oEvent) {
 			var button = oEvent.getSource();
@@ -126,181 +95,21 @@ sap.ui.define([
 		},
 		OnDatePress: function(oEvent) {
 			return;
-			var source = oEvent.getSource();
-			var headerSeq = source.getParent().getParent().getInitialOrder();
-			//	var Items = source.getParent().getParent().getParent().getItems();
-			var Items = this.getView().byId('table').getItems();
-			if (source.getCustomData()[0].getValue() === "") {
-				source.getCustomData()[0].setValue("S");
-				this.daySelected.push(source);
-				//	this.days.push(source.getCustomData()[1].getValue());
-				source.getParent().getCustomData()[0].setValue("S");
-				for (var k = 0; k < Items.length; k++) {
-					var button = Items[k].getCells()[headerSeq];
-					if (button.getEnabled()) {
-						button.getCustomData()[0].setValue("S");
-						this.selectedBox.push(button);
-						var sDay = button.getCustomData()[3].getValue();
-						/*var empId = button.getCustomData()[2].getValue();
-						var sDay = button.getCustomData()[3].getValue();
-						var index = this._EmployeeIndexInArray(empId); //this.employees.indexOf(empId);
-						if (index === -1) {
-							var data = {
-								employee: empId,
-								Days: [sDay]
-							};
-							this.employees.push(data);
-
-						} else {
-							if (this.employees[index].Days.indexOf(sDay) === -1) {
-								this.employees[index].Days.push(sDay);
-							}
-						}*/
-
-					}
-				}
-				if (this.allEmps !== null) {
-					for (var m = 0; m < this.allEmps.length; m++)
-
-					{
-						var empId = this.allEmps[m].EmployeeId;
-
-						var index = this._EmployeeIndexInArray(empId); //this.employees.indexOf(empId);
-						if (index === -1) {
-							var data = {
-								employee: empId,
-								Days: [sDay]
-							};
-							this.employees.push(data);
-
-						} else {
-							if (this.employees[index].Days.indexOf(sDay) === -1) {
-								this.employees[index].Days.push(sDay);
-							}
-						}
-					}
-
-				}
-			} else {
-				source.getCustomData()[0].setValue("");
-				var index2 = this.daySelected.indexOf(source);
-				this.daySelected.splice(index2, 1);
-
-				source.getParent().getCustomData()[0].setValue("");
-				//var index2 = this.days.indexOf(source.getCustomData()[1].getValue());
-				//this.days.splice(index2, 1);
-
-				for (var j = 0; j < Items.length; j++) {
-					var button1 = Items[j].getCells()[headerSeq];
-					if (button1.getEnabled()) {
-						button1.getCustomData()[0].setValue("");
-						var boxindex = this.selectedBox.indexOf(button1);
-						this.selectedBox.splice(boxindex, 1);
-						var sDay2 = button1.getCustomData()[3].getValue();
-
-						/*var empId2 = button1.getCustomData()[2].getValue();
-						var sDay2 = button1.getCustomData()[3].getValue();
-						var empIndex = this._EmployeeIndexInArray(empId2);
-						var index2 = this.employees[empIndex].Days.indexOf(sDay2);
-						this.employees[empIndex].Days.splice(index2, 1);
-						if (this.employees[empIndex].Days.length === 0) {
-
-							this.employees.splice(empIndex, 1);
-
-						}*/
-
-					}
-				}
-				if (this.allEmps !== null) {
-					for (var n = 0; n < this.allEmps.length; n++) {
-						var empId2 = this.allEmps[n].EmployeeId;
-
-						var empIndex = this._EmployeeIndexInArray(empId2);
-						var index2 = this.employees[empIndex].Days.indexOf(sDay2);
-						this.employees[empIndex].Days.splice(index2, 1);
-						if (this.employees[empIndex].Days.length === 0) {
-
-							this.employees.splice(empIndex, 1);
-
-						}
-					}
-
-				}
-
-			}
 		},
 		OnEmployeePress: function(oEvent) {
-			var empBox = oEvent.getSource();
-			var empId = empBox.getCustomData()[1].getValue();
-			if (empBox.getCustomData()[0].getValue() === "") {
-				empBox.getCustomData()[0].setValue("S");
-				this.selectedBox.push(empBox);
-				empBox.getParent().getCustomData()[0].setValue("S");
-				this.selectedBox.push(empBox.getParent());
-				var index = this._EmployeeIndexInArray(empId);
-				if (index === -1) {
-					var data = {
-						employee: empId,
-						Days: []
-					};
-					this.employees.push(data);
-				}
-				var emphours = empBox.getParent().getParent().getCells();
-				var sDate = [];
-				for (var k = 1; k < 6; k++) {
-
-					var button = emphours[k];
-					if (button.getCustomData()[1].getValue() !== "L") {
-						button.getCustomData()[0].setValue("S");
-						this.selectedBox.push(button);
-						sDate.push(button.getCustomData()[3].getValue());
-					}
-				}
-				if (this.twoWeek) {
-					for (var k = 8; k < 15; k++) {
-
-						var button = emphours[k];
-						if (button.getCustomData()[1].getValue() !== "L") {
-							button.getCustomData()[0].setValue("S");
-							this.selectedBox.push(button);
-							sDate.push(button.getCustomData()[3].getValue());
-						}
-					}
-				}
-
-				var index3 = this._EmployeeIndexInArray(empId);
-				this.employees[index3].Days = sDate;
-			} else {
-				empBox.getCustomData()[0].setValue("");
-				var empindex1 = this.selectedBox.indexOf(empBox);
-				this.selectedBox.splice(empindex1, 1);
-				empBox.getParent().getCustomData()[0].setValue("");
-				var empindex2 = this.selectedBox.indexOf(empBox.getParent());
-				this.selectedBox.splice(empindex2, 1);
-				var index2 = this._EmployeeIndexInArray(empId);
-				this.employees.splice(index2, 1);
-				var emphours1 = empBox.getParent().getParent().getCells();
-				for (var k1 = 1; k1 < 15; k1++) {
-					var button1 = emphours1[k1];
-					if (button1.getCustomData()[1].getValue() !== "L") {
-						var boxindex = this.selectedBox.indexOf(button1);
-						this.selectedBox.splice(boxindex, 1);
-						button1.getCustomData()[0].setValue("");
-					}
-				}
-			}
+			return;
 		},
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf com.vinci.timesheet.admin.view.TimesheetSelection
+		 * @memberOf com.vinci.timesheet.admin.view.TeamManage
 		 */
 		//		onBeforeRendering: function() {
 		//		},
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf com.vinci.timesheet.admin.view.TimesheetSelection
+		 * @memberOf com.vinci.timesheet.admin.view.TeamManage
 		 */
 		onAfterRendering: function() {
 			var totalH = window.innerHeight - 200;
@@ -309,7 +118,7 @@ sap.ui.define([
 		},
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf com.vinci.timesheet.admin.view.TimesheetSelection
+		 * @memberOf com.vinci.timesheet.admin.view.TeamManage
 		 */
 		//	onExit: function() {
 		//
@@ -421,20 +230,44 @@ sap.ui.define([
 					thatControl.buildSuccTeamItem(oData);
 				},
 				error: function(oError) {
-					//
+					thatControl.buildErrTeamItem();
 				}
 			};
 			this.getView().getModel().read("/TeamEmployeeSet", mParameters);
 		},
 		buildSuccTeamItem: function(data) {
-
+			var tableItems = this.byId("table").getItems();
+			for (var i = 0; i < tableItems.length; i++) {
+				var tableItemCells = tableItems[i].getCells();
+				for (var e = 1; e < tableItemCells.length; e++) {
+					var localEmpId = tableItemCells[e].data().employee;
+					var localTeamId = tableItemCells[e].data().selectedTeam;
+					var localBusinessUnit = this.userPref.defaultBU;
+					var localUserId = this.userPref.userID;
+					var path = "/TeamEmployeeSet(UserId='" + localUserId +
+						"',BusinessUnit='" + localBusinessUnit +
+						"',TeamId='" + localTeamId + "',EmpId='" + localEmpId + "')";
+					var localTeamAssigned = this.getView().getModel().getProperty(path).TeamAssigned;
+					if (localTeamAssigned === true) {
+						tableItemCells[e].setIcon("sap-icon://color-fill");
+					} else {
+						tableItemCells[e].setIcon("sap-icon://border");
+					}
+				}
+			}
+		},
+		buildErrTeamItem: function() {
+			var tableItems = this.byId("table").getItems();
+			for (var i = 0; i < tableItems.length; i++) {
+				var tableItemCells = tableItems[i].getCells();
+				for (var e = 1; e < tableItemCells.length; e++) {
+					tableItemCells[e].setIcon("sap-icon://border");
+				}
+			}
 		},
 		_applyFilters: function() {
 			var oTable = this.byId("table");
 			var Filters = [
-				// new Filter("WeekNumber", FilterOperator.EQ, this.currentWeekNumber),
-				// new Filter("WeekYear", FilterOperator.EQ, this.currentYear),
-				// new Filter("isByWeekly", FilterOperator.EQ, this.twoWeek),
 				new Filter("BusinessUnit", FilterOperator.EQ, this.userPref.defaultBU)
 			];
 			if (this.userPref.employeeFilter !== null && this.userPref.employeeFilter.length > 0) {
@@ -456,56 +289,25 @@ sap.ui.define([
 			this.teamBinding();
 			this.employees = this.getView().getModel("employeeDaysSelected").getData();
 			if (argument.source === 'Summary') {
-
 				this.refresh = true;
-
-			}
-
-			/*this.employees = [];
-			this.byId("table").getBinding("items").refresh();*/
-
-		},
-		storeAllEmployee: function() {
-			if (this.allEmps === null) {
-				var Filters = [new Filter("BusinessUnit", FilterOperator.EQ, this.userPref.defaultBU),
-					new Filter("IsTimeEntryEnable", FilterOperator.EQ, true)
-				];
-
-				if (this.userPref.employeeFilter !== null && this.userPref.employeeFilter.length > 0) {
-					Filters.push(new Filter("FullName", FilterOperator.Contains, this.userPref.employeeFilter));
-				}
-				var that = this;
-				this.getModel().read("/EmployeeSet", {
-					filters: Filters,
-					urlParameters: {
-						"$select": 'EmployeeId'
-					},
-					success: function(odata) {
-						that.allEmps = odata.results;
-						/*if (that.totalEmp !== odata.results.length)
-							MessageBox.alert(that.getResourceBundle().getText("alertNotAllEmp"));*/
-					}
-
-				});
 			}
 		},
 		/**
-		 *@memberOf com.vinci.timesheet.admin.controller.TimesheetSelection
+		 *@memberOf com.vinci.timesheet.admin.controller.TeamManage
 		 */
 		onPressCancel: function() {
 
 			this.getRouter().navTo("home", {}, true);
 		},
 		/**
-		 *@memberOf com.vinci.timesheet.admin.controller.TimesheetSelection
+		 *@memberOf com.vinci.timesheet.admin.controller.TeamManage
 		 */
 		onManageTeamEntry: function(oEvent) {
 			var requestBody = {
 				"UserId": this.userPref.userID,
 				"BusinessUnit": this.userPref.defaultBU,
-				"TeamId": this.employeId,
-				"TeamName": "Team",
-				"ProcessingMode": "C",
+				"TeamId": "Team",
+				"ProcessingMode": "TIC", // TIC :- Team  Item Create
 				"NavTeamEmployees": []
 			};
 			var tableItems = this.byId("table").getItems();
@@ -543,14 +345,6 @@ sap.ui.define([
 		},
 		onManageTeamName: function(oEvent) {
 
-		},
-		_EmployeeIndexInArray: function(empId) {
-			for (var k = 0; k < this.employees.length; k++) {
-				if (this.employees[k].employee === empId)
-					return k;
-			}
-			return -1;
 		}
-
 	});
 });
