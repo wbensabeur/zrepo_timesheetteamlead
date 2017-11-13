dba6f065c81c4912833f732e1e690ea852a5615d
Rajiv Shivdev Pandey <rajiv.shivdev.pandey@sap.com>
2017-11-13 07:00:14 +0000

    Update for delete confirmation message

--- webapp/controller/AddTimesheet.controller.js
+++ webapp/controller/AddTimesheet.controller.js
@@ -186,7 +186,7 @@ sap.ui.define([
 
 		//// **AddProjectTime Fragment Event** ///////
 		OnTimeDelete: function(oEvent) {
-			var that = this;
+			/*var that = this;
 			MessageBox.confirm(
 			that.getResourceBundle().getText("confirmDeleteMsg"), 
 			{
@@ -200,7 +200,10 @@ sap.ui.define([
 						return;
 					}
 				}
-			});
+			});*/
+			//var timeModel = this.getView().getModel('AddTime');
+			var container = this.getView().byId('addTimeTab').getItems()[0];
+			fragment.AddProjectTime_OnTimeDelete(oEvent, container);
 		},
 		OnchangeTimeSelection: function(oEvent) {
 			//	var timeModel = this.getView().getModel('AddTime');
