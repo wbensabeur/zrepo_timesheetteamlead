787afd72fd256069ddfe23e91f5fd8923c2ae790
Vijay Singh Rajput <vijay.singh.rajput@sap.com>
2018-01-11 06:01:45 +0000

    logout fix

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,34 +1,55 @@
 sap.ui.define([
-		"com/vinci/timesheet/admin/controller/BaseController",
-		"sap/ui/model/json/JSONModel"
-	], function (BaseController, JSONModel) {
-		"use strict";
+	"com/vinci/timesheet/admin/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel) {
+	"use strict";
 
-		return BaseController.extend("com.vinci.timesheet.admin.controller.App", {
+	return BaseController.extend("com.vinci.timesheet.admin.controller.App", {
 
-			onInit : function () {
-				var oViewModel,
-					fnSetAppNotBusy,
-					iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
+		onInit: function() {
+			var oViewModel,
+				fnSetAppNotBusy,
+				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
 
-				oViewModel = new JSONModel({
-					busy : true,
-					delay : 0
-				});
-				this.setModel(oViewModel, "appView");
+			oViewModel = new JSONModel({
+				busy: true,
+				delay: 0
+			});
+			this.setModel(oViewModel, "appView");
 
-				fnSetAppNotBusy = function() {
-					oViewModel.setProperty("/busy", false);
-					oViewModel.setProperty("/delay", iOriginalBusyDelay);
-				};
+			fnSetAppNotBusy = function() {
+				oViewModel.setProperty("/busy", false);
+				oViewModel.setProperty("/delay", iOriginalBusyDelay);
+			};
 
-				this.getOwnerComponent().getModel().metadataLoaded().
-					then(fnSetAppNotBusy);
+			this.getOwnerComponent().getModel().metadataLoaded().
+			then(fnSetAppNotBusy);
 
-				// apply content density mode to root view
-				//this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-			}
-		});
+			var logout = sap.ui.getCore().byId("shellLogout");
+			logout.attachPress(function(oEvent) {
 
-	}
-);
+				var hostname = window.location.origin;
+				var logoutpage = false;
+				var SAMLLogoffURL = hostname + '/sap/saml2/sp/slo/300';
+				var myPopup = window.open(SAMLLogoffURL, 'logoff', 'left=20,top=20,width=100,height=100,resizable=0');
+				if (!sap.ui.Device.browser.msie) {
+					myPopup.onload = function() {
+						logoutpage = true;
+						myPopup.close();
+						window.location.reload(true);
+					}
+				}
+				setTimeout(function() {
+					if (!logoutpage) {
+						myPopup.close();
+						window.location.reload(true);
+					}
+				}, 1500);
+			});
+
+			// apply content density mode to root view
+			//this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		}
+	});
+
+});
